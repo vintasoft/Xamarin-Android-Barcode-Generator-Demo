d3d5fe9140be2dd49cc088bfef0283b2ab667462
vintasoft <D:\The Bat!>
2020-08-12 12:50:11 +0300

    Source codes update

--- CSharp/VintasoftXamarinBarcodeLicense.cs
+++ CSharp/VintasoftXamarinBarcodeLicense.cs
@@ -13,7 +13,7 @@
         {
             // ==========================================
             // Register the evaluation version of VintaSoft Barcode .NET SDK:
-            Vintasoft.XamarinBarcode.BarcodeGlobalSettings.Register("Alex", "alex@vintasoft.local", "2020-08-09", "nnZV/dxtuz/3uVxCgl+ycONHHR4rp5sglLj3O1ewoMwXY9hnq5qY57Y+xKKv8n9GngeK/2ySCOEt6LsYdAen0nJGztq0uhSbMX8xyuTloevaCHSUpkYCW1nIHw9+902ACcrjk6v3d4gYTbMh+79gIbsr7afiGUO37/p3HvUD6gY");
+            // Vintasoft.XamarinBarcode.BarcodeGlobalSettings.Register(user, email, expirationDate, regCode);
             // ==========================================
         }
 
